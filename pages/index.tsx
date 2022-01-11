e5d513e80143ed849f531045c8cb954215fa4065
Astrect <astrecttv@gmail.com>
2022-01-11 00:01:20 -0600

    update join community section position.

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,8 +15,8 @@ export default function Home() {
          <NavigationMenu />
          <HeroSection />
          <TokenStats />
-         <AboutSection />
          <CommunitySection />
+         <AboutSection />
          <GoalsSection />
          <NFTSection />
          <PartnersSection />
