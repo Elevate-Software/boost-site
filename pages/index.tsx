0b86fabbf9b38064bbb266547106c75e860c2941
Astrect <astrecttv@gmail.com>
2022-01-11 21:50:46 -0600

    add favicon.

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,19 +8,27 @@ import {AboutSection} from "../components/sections/about";
 import {GoalsSection} from "../components/sections/goals";
 import {NFTSection} from "../components/sections/nft";
 import {CommunitySection} from "../components/sections/community";
+import { Fragment } from 'react';
 
 export default function Home() {
   return (
-     <main>
-         <NavigationMenu />
-         <HeroSection />
-         <TokenStats />
-         <CommunitySection />
-         <AboutSection />
-         <GoalsSection />
-         <NFTSection />
-         <PartnersSection />
-         <Footer />
-     </main>
+      <Fragment>
+          <Head>
+              <title>Boost2.0</title>
+              <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+              <link rel="icon" type="image/png" sizes="32x32" href="/favicon.png"/>
+          </Head>
+          <main>
+              <NavigationMenu />
+              <HeroSection />
+              <TokenStats />
+              <CommunitySection />
+              <AboutSection />
+              <GoalsSection />
+              <NFTSection />
+              <PartnersSection />
+              <Footer />
+          </main>
+      </Fragment>
   )
 }
