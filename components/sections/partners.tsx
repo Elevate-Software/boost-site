cdaba0676048a2cbb7ff7374cd9939ddf5a3e458
Astrect <astrecttv@gmail.com>
2022-01-11 20:57:23 -0600

    fix coinmarketcap logo.

--- components/sections/partners.tsx
+++ components/sections/partners.tsx
@@ -6,7 +6,7 @@ export const PartnersSection = () => (
             <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:px-8">
                 <div className="grid grid-cols-2 gap-8 md:grid-cols-6 lg:grid-cols-5">
                     <div className="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
-                        <img className="h-12" src="https://cdn.discordapp.com/attachments/913192688327753758/930343685059649556/cmc_1.png" alt="Tuple" />
+                        <img className="h-12" src="https://cdn.discordapp.com/attachments/913192688327753758/930655404940214302/cmc2.png" alt="Tuple" />
                     </div>
                     <div className="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
                         <img className="h-12" src="https://cdn.discordapp.com/attachments/913192688327753758/930326352555544586/etherscan-logo-light.png" alt="Mirage" />
