f2f4a6e37b9b8de6f25aeb69d18bea647164fb3e
Chase Brown <Chasebrown7@icloud.com>
2022-04-19 13:17:06 -0700

    staking2

--- components/tokenstats/tokenstats.tsx
+++ components/tokenstats/tokenstats.tsx
@@ -19,7 +19,7 @@ export const TokenStats = () => (
                     </div>
                     <div className="col-span-1 flex justify-center md:col-span-2 lg:col-span-1">
                         <div className="px-4 py-5 overflow-hidden sm:p-6">
-                            <div className="text-sm font-medium text-gray-400 truncate">Launch</div>
+                            <div className="text-sm font-medium text-gray-400 truncate">Staking</div>
                             <div className="mt-1 text-3xl font-semibold text-white">SOON</div>
                         </div>
                     </div>
